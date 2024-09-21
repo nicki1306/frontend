c6c6aa3dfd151fbc80d4f979b09bce0ce17e3b8d
Nicole <nickii.cn@hotmail.com>
2024-09-21 12:19:48 -0300

    cambios deploy

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,7 @@
 import { defineConfig } from 'vite'
-
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-
 export default defineConfig({
     plugins: [react()],
-    base: './',
-    build: {
-        outDir: 'dist',
-        rollupOptions: {
-            input: './index.html'
-        }
-    },
-    server: {
-        host: '0.0.0.0',
-        port: 3000,
-        proxy: process.env.NODE_ENV === 'development' ? {
-            '/api': {
-                target: 'http://localhost:8081',
-                changeOrigin: true
-            }
-        } : {}
-    }
-});
+})
